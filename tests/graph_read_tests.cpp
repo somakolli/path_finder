c48e54fbf167a3de9aab41c9eccf9022359effbf
Sokol Makolli <st116214@stud.uni-stuttgart.de>
2020-07-31 11:24:15 +0200

    add graph tests

---
+++ tests/graph_read_tests.cpp
@@ -0,0 +1,94 @@
+#include <gtest/gtest.h>
+#include <path_finder/graphs/Graph.h>
+#include <path_finder/storage/GraphReader.h>
+
+namespace pathFinder {
+namespace GraphTest {
+const std::string PATH = "../test-data/";
+
+TEST(Graph, ReadWorks) {
+  Graph graph;
+  GraphReader::readFmiFile(graph, PATH + "test.fmi");
+  auto edges = graph.edgesFor(1);
+  auto edge0 = Edge(*edges.begin());
+  auto edge1 = Edge(*(edges.begin() + 1));
+  auto edge2 = Edge(*(edges.begin() + 2));
+  ASSERT_EQ(edge0.source, 1);
+  ASSERT_EQ(edge0.target, 0);
+  ASSERT_EQ(edge1.source, 1);
+  ASSERT_EQ(edge1.target, 2);
+  ASSERT_EQ(edge2.source, 1);
+  ASSERT_EQ(edge2.target, 7);
+}
+TEST(CHGraph, ReadWorks) {
+  CHGraph graph;
+  GraphReader::readCHFmiFile(graph, PATH + "test.chfmi", false);
+  auto edges = graph.edgesFor(1, EdgeDirection::FORWARD);
+  auto edge0 = CHEdge(*edges.begin());
+  auto edge1 = CHEdge(*(edges.begin() + 1));
+  auto edge2 = CHEdge(*(edges.begin() + 2));
+  auto edge3 = CHEdge(*(edges.begin() + 3));
+  auto edge4 = CHEdge(*(edges.begin() + 4));
+  auto edge5 = CHEdge(*(edges.begin() + 5));
+  ASSERT_EQ(edge0.source, 1);
+  ASSERT_EQ(edge0.target, 0);
+  ASSERT_EQ(edge1.source, 1);
+  ASSERT_EQ(edge1.target, 2);
+  ASSERT_EQ(edge2.source, 1);
+  ASSERT_EQ(edge2.target, 3);
+  ASSERT_EQ(edge3.source, 1);
+  ASSERT_EQ(edge3.target, 5);
+  ASSERT_EQ(edge4.source, 1);
+  ASSERT_EQ(edge4.target, 6);
+  ASSERT_EQ(edge5.source, 1);
+  ASSERT_EQ(edge5.target, 7);
+}
+TEST(CHGraph, ReadFromDiskWorks) {
+  CHGraph graph;
+  GraphReader::readCHFmiFile(graph, PATH + "test.chfmi", false);
+  auto edges = graph.edgesFor(1, EdgeDirection::FORWARD);
+  auto edge0 = CHEdge(*edges.begin());
+  auto edge1 = CHEdge(*(edges.begin() + 1));
+  auto edge2 = CHEdge(*(edges.begin() + 2));
+  auto edge3 = CHEdge(*(edges.begin() + 3));
+  auto edge4 = CHEdge(*(edges.begin() + 4));
+  auto edge5 = CHEdge(*(edges.begin() + 5));
+  ASSERT_EQ(edge0.source, 1);
+  ASSERT_EQ(edge0.target, 0);
+  ASSERT_EQ(edge1.source, 1);
+  ASSERT_EQ(edge1.target, 2);
+  ASSERT_EQ(edge2.source, 1);
+  ASSERT_EQ(edge2.target, 3);
+  ASSERT_EQ(edge3.source, 1);
+  ASSERT_EQ(edge3.target, 5);
+  ASSERT_EQ(edge4.source, 1);
+  ASSERT_EQ(edge4.target, 6);
+  ASSERT_EQ(edge5.source, 1);
+  ASSERT_EQ(edge5.target, 7);
+}
+TEST(CHGraph, GridWorks) {
+  CHGraph graph;
+  GraphReader::readCHFmiFile(graph, PATH + "test.chfmi", true);
+  auto edges = graph.edgesFor(1, EdgeDirection::FORWARD);
+  auto edge0 = CHEdge(*edges.begin());
+  auto edge1 = CHEdge(*(edges.begin() + 1));
+  auto edge2 = CHEdge(*(edges.begin() + 2));
+  auto edge3 = CHEdge(*(edges.begin() + 3));
+  auto edge4 = CHEdge(*(edges.begin() + 4));
+  auto edge5 = CHEdge(*(edges.begin() + 5));
+  ASSERT_EQ(edge0.source, 1);
+  ASSERT_EQ(edge0.target, 0);
+  ASSERT_EQ(edge1.source, 1);
+  ASSERT_EQ(edge1.target, 2);
+  ASSERT_EQ(edge2.source, 1);
+  ASSERT_EQ(edge2.target, 3);
+  ASSERT_EQ(edge3.source, 1);
+  ASSERT_EQ(edge3.target, 5);
+  ASSERT_EQ(edge4.source, 1);
+  ASSERT_EQ(edge4.target, 6);
+  ASSERT_EQ(edge5.source, 1);
+  ASSERT_EQ(edge5.target, 7);
+}
+}
+
+}
