8c051e1255ed3d41828c0599b0b90a5bfd3d9c78
Sokol Makolli <st116214@stud.uni-stuttgart.de>
2020-08-12 17:25:01 +0200

    Revert "Merge remote-tracking branch 'origin/alloca' into alloca"
    This reverts commit a7af312e953c2081a5e2f1c3cb1e6d21ceca7cf3.

--- include/path_finder/routing/HybridPathFinder.h
+++ include/path_finder/routing/HybridPathFinder.h
@@ -241,8 +241,11 @@ HybridPathFinder<Graph, CellIdStore>::calcLabelHybrid(
     NodeId source, EdgeDirection direction, CalcLabelTimingInfo& calcLabelTimingInfo) {
   Stopwatch stopwatch;
   if (m_graph->getLevel(source) >= m_labelsUntilLevel && m_hubLabelsCalculated) {
-    auto vec = m_hubLabelStore->retrieve(source, direction);
-    calcLabelTimingInfo.lookUpTime += stopwatch.elapsedMicro();
+    const auto &sourceLabel = m_hubLabelStore->retrieve(source, direction);
+    costNodeVec_t vec;
+    for (const auto entry : sourceLabel)
+      vec.push_back(entry);
+    calcLabelTimingInfo.lookUpTime = stopwatch.elapsedMicro();
     return vec;
   }
 
@@ -285,12 +288,15 @@ HybridPathFinder<Graph, CellIdStore>::calcLabelHybrid(
     }
   }
   Static::sortLabel(settledNodes);
-  calcLabelTimingInfo.graphSearchTime += stopwatch.elapsedMicro();
+  calcLabelTimingInfo.graphSearchTime = stopwatch.elapsedMicro();
   Stopwatch stopwatch1;
   for (auto [previousNodeId, labelsToCollect] : labelsToCollectMap) {
     for (auto id : labelsToCollect) {
       stopwatch1.reset();
-      costNodeVec_t targetLabel = m_hubLabelStore->retrieve(id, direction);
+      costNodeVec_t targetLabel;
+      auto label = m_hubLabelStore->retrieve(id, direction);
+      for (const auto &entry : label)
+        targetLabel.push_back(entry);
       calcLabelTimingInfo.lookUpTime += stopwatch1.elapsedMicro();
       stopwatch1.reset();
       costNodeVec_t resultVec;
