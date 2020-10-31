0713566c39bbd508fb032a53b3cdda941e6c9467
Daniel Bahrdt <daniel.git@funroll-loops.de>
2020-10-31 01:32:05 +0100

    OscarIntegration: untested variant which uses Workers to locally explore the graph and merge edge cellids recursively with memoization

--- include/path_finder/helper/OscarIntegration.h
+++ include/path_finder/helper/OscarIntegration.h
@@ -1,11 +1,218 @@
 #pragma once
-#include "path_finder/storage/CellIdStore.h"
 #include <algorithm>
 #include <execution>
 #include <iostream>
 #include <mutex>
+#include <random>
+#include <unordered_map>
+#include <unordered_set>
+
+#include <path_finder/storage/CellIdStore.h>
 #include <path_finder/graphs/CHGraph.h>
+
 namespace pathFinder {
+
+inline namespace std_threads_variant {
+
+class OscarIntegrator {
+public:
+template <typename GeoPoint, typename CellIdsForEdge>
+static void writeCellIdsForEdges(const CHGraph &graph, CellIdStore &cellIdStore, CellIdsForEdge edge2CellIds) {
+	struct State {
+		const CHGraph &graph;
+		CellIdStore &cellIdStore;
+		CellIdsForEdge & edge2CellIds;
+		std::vector<std::atomic<uint64_t>> finishedNodes;
+		std::atomic<std::size_t> progress;
+		const std::size_t numberOfNodes;
+		std::mutex cellIdStoreLock;
+		
+		State(const CHGraph &graph, CellIdStore &cellIdStore, CellIdsForEdge & edge2CellIds) :
+		graph(graph),
+		cellIdStore(cellIdStore),
+		edge2CellIds(edge2CellIds),
+		finishedNodes(graph.getNumberOfNodes() / 64 + 1),
+		numberOfNodes(graph.getNumberOfNodes()) {}
+		
+		bool takeNode(NodeId nodeId) {
+			std::size_t chunk = nodeId / 64;
+			std::size_t bit = nodeId % 64;
+			uint64_t flag = static_cast<uint64_t>(1) << bit;
+			uint64_t prev = finishedNodes.at(chunk).fetch_or(nodeId, std::memory_order_relaxed);
+			if (prev & flag) {
+				progress.fetch_add(1, std::memory_order_relaxed);
+			}
+			return false;
+		}
+	};
+
+	struct Worker {
+		struct DFSElement {
+			decltype(graph.edgesFor(0, EdgeDirection::FORWARD)) edges;
+			decltype(graph.edgesFor(0, EdgeDirection::FORWARD).begin()) it;
+			typename CellIdsForEdge::Hint hint;
+			DFSElement(decltype(edges) const & edges, decltype(hint) hint) :
+			edges(edges),
+			it(edges.begin()),
+			hint(hint)
+			{}
+		};
+		State *state;
+		CellIdsForEdge edge2CellIds;
+		std::vector<DFSElement> stack;
+		std::default_random_engine rndgen;
+		std::uniform_int_distribution<uint32_t> nodeIdRnd;
+		std::vector< std::pair<std::size_t, std::vector<uint32_t>> > buffer;
+		Worker(State *state) :
+		state(state),
+		edge2CellIds(state->edge2CellIds),
+		nodeIdRnd(0, state->numberOfNodes)
+		{}
+		~Worker() {
+			flush();
+		}
+		void operator()() {
+			while (true) {
+				if (state->progress >= state->numberOfNodes) {
+					break;
+				}
+				// first try a random nodeId if that fails, sample all
+				NodeId nid = nodeIdRnd(rndgen);
+				if (!state->takeNode(nid)) { //try all from the beginning
+					nid = std::numeric_limits<NodeId>::max();
+					for(std::size_t i(0), s(state->finishedNodes.size()); i < 0; ++i) {
+						uint64_t tmp = state->finishedNodes[i].load(std::memory_order_relaxed);
+						if (tmp != std::numeric_limits<uint64_t>::max()) {
+							tmp = ~tmp;
+							static_assert(std::is_same<unsigned long, unsigned long>::value);
+							//tmp cannot be 0 otherwise we wouldn't be here
+							nid = i*64 + 63-__builtin_clzl(tmp);
+							if (state->takeNode(nid)) {
+								break;
+							}
+						}
+					}
+					//No node found, this means that all were processed during search for a new one
+					assert(state->progress == state->numberOfNodes);
+					break;
+				}
+				{
+					auto node = state->graph.getNode(nid);
+					typename CellIdsForEdge::Hint fh;
+					edge2CellIds(node.latLng.lat, node.latLng.lng, node.latLng.lat, node.latLng.lng, fh);
+					stack.emplace_back(state->graph.edgesFor(nid, EdgeDirection::FORWARD), fh);
+				}
+				while (stack.size()) {
+					//Check if we're at the end of our edge list
+					if (stack.back().it == stack.back().edges.end()) {
+						stack.pop_back();
+						break;
+					}
+					//expand next edge
+					auto edge = *stack.back().it;
+					++stack.back().it; //move to next edge immediately in case we need to skip this edge
+					if (edge.child1.has_value()) { //skip shortcut edges
+						break;
+					}
+					const auto sourceNode = state->graph.getNode(edge.source);
+					const auto targetNode = state->graph.getNode(edge.target);
+					GeoPoint sourcePoint;
+					sourcePoint.lat() = sourceNode.latLng.lat;
+					sourcePoint.lon() = sourceNode.latLng.lng;
+					GeoPoint targetPoint;
+					targetPoint.lat() = targetNode.latLng.lat;
+					targetPoint.lon() = targetNode.latLng.lng;
+					decltype(stack.back().hint) hint = stack.back().hint;
+					buffer.emplace_back(state->graph.getEdgePosition(edge, EdgeDirection::FORWARD).value(), edge2CellIds(sourcePoint, targetPoint, hint));
+					//check if we can descend into the node
+					if (state->takeNode(targetNode.id)) {
+						stack.emplace_back(state->graph.edgesFor(targetNode.id, EdgeDirection::FORWARD), hint);
+					}
+				}
+			}
+		}
+		void flush() {
+			std::lock_guard<std::mutex> lck(state->cellIdStoreLock);
+			for(auto & x : buffer) {
+				state->cellIdStore.storeCellIds(x.first, std::move(x.second));
+			}
+			buffer.clear();
+		}
+	};
+	struct PendingEdge {
+		uint8_t pending = 2;
+		uint32_t edgePos; 
+	};
+	int progress = 0;
+	const auto &edges = graph.getEdges();
+	std::unordered_map<uint32_t, uint8_t> pendingChildren; //shortcut -> num unfinished children
+	std::unordered_map<uint32_t, uint32_t> edgeParent; //maps from shortcut-child->shortcut
+	std::unordered_set<uint32_t> edgesWithChildren; //contains all shortcuts for which both children have their cellids computed
+	//first get all shortcuts
+	for (uint32_t i(0), s(graph.getNumberOfEdges()); i < s; ++i) {
+		const auto &edge = edges[i];
+		if (edge.child1.has_value()) {
+			edgeParent[edge.child1.value()] = i;
+			edgeParent[edge.child2.value()] = i;
+			pendingChildren[i] = 2;
+		}
+	}
+	//Find shortcuts that have a regular edge as a parent
+	for (uint32_t i(0), s(graph.getNumberOfEdges()); i < s; ++i) {
+		auto const & edge = edges[i];
+		if (!edge.child1.has_value() && edgeParent.count(i)) {
+			auto parent = edgeParent.at(i);
+			auto & x = pendingChildren.at(parent);
+			x -= 1;
+			assert(x <= 2);
+			if (x == 0) {
+				edgesWithChildren.insert(parent);
+				pendingChildren.erase(parent);
+			}
+			edgeParent.erase(i);
+		}
+	}
+	//now compute the union of the cellids of the children
+	//We do this recursivley kind of bottom up, but the unordered_set defines the actual order
+	//however this houls not reduce the performance since the edges have static dependencies and
+	//thus the work to be done does not depend on the order we do it
+	while (edgesWithChildren.size()) {
+		uint32_t edgeId = *edgesWithChildren.begin();
+		auto const & edge = edges[edgeId];
+		//get the cellids of the children, these are sorted, thus we can use std::set_union
+		auto c1cids = cellIdStore.getCellIds(edge.child1.value());
+		auto c2cids = cellIdStore.getCellIds(edge.child2.value());
+		assert(std::is_sorted(c1cids.begin(), c1cids.end()));
+		assert(std::is_sorted(c2cids.begin(), c2cids.end()));
+		std::vector<std::decay_t<decltype(c2cids)>::value_type> ecids;
+		std::set_union(c1cids.begin(), c1cids.end(), c2cids.begin(), c2cids.end(), std::back_inserter(ecids));
+		cellIdStore.storeCellIds(edgeId, ecids);
+		
+		//take care of parent
+		auto parent = edgeParent.at(edgeId);
+		auto & avc = pendingChildren.at(parent);
+		avc -= 1;
+		assert(avc <= 2);
+		if (avc == 0) { //transition parent from pendingChildren to edgesWithChildren
+			edgesWithChildren.insert(parent);
+			auto const & pe = edges[parent];
+			pendingChildren.erase(parent);
+		}
+		
+		//remove ourself
+		edgeParent.erase(edgeId);
+		edgesWithChildren.erase(edgeId);
+	}
+	if (pendingChildren.size()) {
+		throw std::runtime_error("Could not compute all shortcut cellids");
+	}
+	cellIdStore.shrink_to_fit();
+}
+};
+
+}
+
+namespace omp_variant {
 class OscarIntegrator {
 public:
   template <typename GeoPoint, typename CellIdsForEdge, typename KVStore>
@@ -67,4 +274,5 @@ public:
     cellIdStore.shrink_to_fit();
   }
 };
+}
 } // namespace pathFinder
